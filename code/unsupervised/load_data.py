30a3e18fb79e039ed3545f4dfba1a6ac6ac6a9a2
RJ Tayal <rvtayal@gmail.com>
2021-10-20 20:33:44 -0400

    pca pipeline and pca data folder created

--- code/unsupervised/load_data.py
+++ code/unsupervised/load_data.py
@@ -2,12 +2,11 @@ from sklearn.decomposition import PCA
 import numpy as np
 from PIL import Image
 import os
-import pickle
+import tqdm
 
 datapath = os.path.join(os.path.join("..", ".."), "data")
-normalization_size = (400, 400)
 
-def read_data(size = (400, 400)):
+def load_data(size = (400, 400)):
     # load training data
     training_path = os.path.join (datapath, 'train')
     pn_path = os.path.join(training_path, "PNEUMONIA")
@@ -15,12 +14,19 @@ def read_data(size = (400, 400)):
     filelist_p = [f for f in os.listdir(pn_path) if os.path.isfile(os.path.join(pn_path, f))]
     filelist_n = [f for f in os.listdir(n_path) if os.path.isfile(os.path.join(n_path, f))]
 
-    # read in NORMAL and PNEUMONIA    
-    images_pn =  np.array([np.array(Image.open(os.path.join(pn_path, fname)).convert('L').resize(size)) for fname in filelist_p])
-    images_n =  np.array([np.array(Image.open(os.path.join(n_path, fname)).convert('L').resize(size)) for fname in filelist_n])
+    # read in NORMAL and PNEUMONIA
+    images_pn = np.zeros((len(filelist_p), size[0]*size[1])) 
+    images_n = np.zeros((len(filelist_n), size[0]*size[1])) 
+    
+    print("Reading PNEUMONIA images")
+    for i in tqdm.tqdm(range(len(filelist_p))):
+        im = Image.open(os.path.join(pn_path, filelist_p[i])).convert('L').resize(size)
+        images_pn[i, :] = np.array(im).reshape((1, -1))
 
-    images_pn = images_pn.reshape((-1, size[0]*size[1]))
-    images_n = images_n.reshape((-1, size[0]*size[1]))
+    print("Reading NORMAL images")
+    for i in tqdm.tqdm(range(len(filelist_n))):
+        im = Image.open(os.path.join(n_path, filelist_n[i])).convert('L').resize(size)
+        images_n[i, :] = np.array(im).reshape((1, -1))
 
     # append labels
     images_n = np.concatenate((images_n, np.zeros((images_n.shape[0], 1))), axis=1)
@@ -31,19 +37,7 @@ def read_data(size = (400, 400)):
 
     total_data = np.concatenate((images_pn, images_n), axis=0)
 
-    outfile = "data_train_{}_{}.pkl".format(size[0], size[1])
-    file = open(os.path.join(datapath, outfile), 'wb')
-    file.close()
-    pickle.dump(total_data, file)
-
-    return os.path.join(datapath, outfile)
-
-
-def load_from_pickle(file):
-    infile = open(file,'rb')
-    data = pickle.load(infile)
-    infile.close()
-    return data
+    return total_data
 
 if __name__ == "__main__":
-    read_data(size=(2,2))
+    data = load_data()
---
+++ code/unsupervised/pca.py
@@ -0,0 +1,28 @@
+from numpy.core.numeric import full
+from sklearn.decomposition import PCA
+import numpy as np
+from load_data import load_data
+import os
+
+save_dir = os.path.join("..", "..", "data", "pca_transformed_data")
+
+def pca_transformation(data,n_comp):
+    data_no_labels = data[:,:-1]
+    pca = PCA(n_comp)
+    transformed_data = pca.fit_transform(data_no_labels)
+    print(np.sum(pca.explained_variance_ratio_))
+    return np.hstack((transformed_data,data[:,-1].reshape((len(data),1))))
+
+def pca_pipline(data, resize=(400,400), n_comp=100, save=True):
+    data = load_data(resize)
+    pca_trans = pca_transformation(data, n_comp)
+    if save:
+        filename = "pca_data_{}_{}_{}.npy".format(resize[0], resize[1], n_comp)
+        full_save_path = os.path.join(save_dir, filename)
+        with open(full_save_path, 'wb') as f:
+            np.save(f, pca_trans)
+        print("Data saved to: {}".format(full_save_path))
+    return pca_trans
+
+if __name__ == "__main__":
+    pca_pipline()
